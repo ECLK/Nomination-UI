3cec3c3b1b845416de8f26c177ad9e2ad594b09b
tharindu99 <madushanka.best@gmail.com>
2018-12-19 17:47:21 +0530

    project init

---
+++ src/app/ProtectedApp.jsx
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Redirect, Route, Switch } from 'react-router-dom';
+import Dashboard from '../pages/Dashboard/Dashboard';
+
+export default class Protected extends Component {
+    render() {
+        return (
+            /* app level routers needs to handle here*/
+            <div>
+                <Switch>
+                    <Redirect exact from='/' to='/dashboard' />
+                    <Route path='/dashboard' component={Dashboard} />
+                </Switch>
+            </div>
+        );
+
+    }
+}
---
+++ src/pages/index.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { BrowserRouter as Router, Redirect, Route, Switch } from 'react-router-dom';
+import { withStyles } from '@material-ui/core/styles';
+import withRoot from 'withRoot';
+import Loadable from 'react-loadable';
+import Progress from 'components/Progress/Progress';
+import Login from 'pages/Login/Login';
+
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
+
+const LoadableProtectedApp = Loadable({
+    loader: () =>
+        import(
+            '../app/ProtectedApp'),
+    loading: Progress,
+});
+
+class Index extends React.Component {
+  state = {
+    open: false,
+  };
+
+  constructor(props) {
+      super(props);
+      this.state = {
+          user: {},
+      };
+  };
+
+
+  render() {
+    const { classes } = this.props;
+    const { user } = this.state;
+
+    return (
+      <div className={classes.root}>
+          <Router basename='/election'>
+              <Switch>
+                  {<Route path='/login' render={props => <Login {...props} updateUser={this.updateUser} />} />}
+                  <Route path='/logout' />
+                  {!user && <Redirect to={{ pathname: '/login'}} />}
+                  <Route render={() => <LoadableProtectedApp user={user} />} />
+              </Switch>
+          </Router>
+      </div>
+    );
+  }
+}
+
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withRoot(withStyles(styles)(Index));
