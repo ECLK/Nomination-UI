3a8a59fdb7f0e3ef7680dc7bfcc8277eebbb7d18
mrclemrkz <awesomeisme007@gmail.com>
2018-12-28 22:17:42 +0530

    update Home to pass dynamic values to InfoBanner component.

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -6,6 +6,7 @@ import MainMenu from 'components/MainMenu/MainMenu';
 import InfoBanner from 'components/InfoBanner/InfoBanner';
 import Axios from 'axios';
 
+
 const drawerWidth = 240;
 
 const styles = theme => ({
@@ -14,12 +15,17 @@ const styles = theme => ({
             paddingLeft: drawerWidth,
             flexShrink: 0,
         },
+    },
+    topBottomSpace: {
+        marginBottom: 15
     }
 });
 
 class Home extends React.Component {
+
     state = {
         open: true,
+        election_id: '43680f3e-97ac-4257-b27a-5f3b452da2e6',
         election: { 
             electionTimeLine: new Array(4).fill(0),
         },
@@ -34,12 +40,13 @@ class Home extends React.Component {
     };
 
     componentDidMount(){
-        Axios.get(`http://localhost:9001/ec-election/elections/43680f3e-97ac-4257-b27a-5f3b452da2e6`)
-            .then(res => {
-                const election = res.data;
-                this.setState({ election });
-                // console.log(this.state);
-            });
+
+        Axios.get(`${process.env.REACT_APP_API_DOMAIN}/ec-election/elections/${this.state.election_id}`)
+        .then(res => {
+            const election = res.data;
+            this.setState({ election });
+            // console.log(this.state);
+        });
     }
 
     render() {
@@ -49,9 +56,15 @@ class Home extends React.Component {
             <div className={classes.root}>
                 <CssBaseline />
                 <MainMenu title="Election Commission of Sri Lanka" ></MainMenu>
+
                 <div className={classes.content}>
+                {/* all the content should go in this.. */}
+
                     <InfoBanner election={this.state.election}></InfoBanner>
+                    
                 </div>
+
+
             </div>
         );
     }
