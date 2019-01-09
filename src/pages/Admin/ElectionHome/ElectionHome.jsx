3b1f3f8fcdf16b82a8ab965596cf4f65730fa9d6
Supun <supunsnnk@gmail.com>
2019-01-09 11:40:55 +0530

    ElectionHome page final update2 01-09

--- src/pages/Admin/ElectionHome/ElectionHome.jsx
+++ src/pages/Admin/ElectionHome/ElectionHome.jsx
@@ -1,24 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import {withStyles} from '@material-ui/core/styles';
-import CssBaseline from '@material-ui/core/CssBaseline';
+
 import MainMenu from 'components/MainMenu/MainMenu';
 import CreateElection from 'components/CreateElection/CreateElection';
 import CallElection from 'components/CallElection/CallElection';
 import ElectionModule from 'components/ElectionModule/ElectionModule';
 import ActiveElection from 'components/ActiveElection/ActiveElection.jsx';
-import Paper from '@material-ui/core/Paper';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-
 import Grid from '@material-ui/core/Grid';
+
+import ExpansionPanel from '@material-ui/core/ExpansionPanel';//---
+import ExpansionPanelDetails from '@material-ui/core/ExpansionPanelDetails';///-
+import ExpansionPanelSummary from '@material-ui/core/ExpansionPanelSummary';
+import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
+
+import Paper from '@material-ui/core/Paper';
+
+
+
+
+
+
 
 const styles = theme => ({
     container: {
         marginLeft: theme.spacing.unit * 35,
         paddingTop: 10,
+    },heading: {
+        fontSize: theme.typography.pxToRem(15),
+        flexBasis: '33.33%',
+        flexShrink: 0,
+
+        fontSize: theme.typography.pxToRem(15),
+        fontWeight: theme.typography.fontWeightRegular,
     },
+    secondaryHeading: {
+        fontSize: theme.typography.pxToRem(15),
+        color: theme.palette.text.secondary,
+    },
+    xxxxx:{
+        wide:'full'
+    }
 
 
 });
@@ -27,22 +50,21 @@ const h1Style = {
 };
 
 class Home extends React.Component {
-    state = {
-        open: true,
+        state = {
+            open: true,
+            expanded:null,
+        };
 
+    handleChange = panel => (event, expanded) => {
+        this.setState({
+            expanded: expanded ? panel : false,
+        });
     };
 
-    handleDrawerOpen = () => {
-        this.setState({open: true});
-    };
-
-    handleDrawerClose = () => {
-        this.setState({open: false});
-    };
 
     render() {
         const {classes} = this.props;
-
+        const { expanded } = this.state;
         return (
             <div>
                 <MainMenu title="Elections Commission of Sri Lanka"></MainMenu>
@@ -60,18 +82,37 @@ class Home extends React.Component {
                         <Grid item xs={12} sm={6}>
                             <CallElection></CallElection>
                         </Grid>
-
                     </Grid>
                     <br />
-                    <Typography variant="h5" component="h2">
-                        Election Module
-                    </Typography>
-                    <ElectionModule></ElectionModule>
-                    <br />
-                    <Typography variant="h5" component="h2">
-                        Active Election
-                    </Typography>
-                    <ActiveElection></ActiveElection>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>Election Module</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails >
+                            <Grid container spacing={24}>
+                                <Grid item xs={12}>
+                                    <Paper className={classes.paper}> <ElectionModule></ElectionModule></Paper>
+                                </Grid>
+                            </Grid>
+
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+                    <ExpansionPanel>
+                        <ExpansionPanelSummary expandIcon={<ExpandMoreIcon />}>
+                            <Typography className={classes.heading}>Active Election</Typography>
+                        </ExpansionPanelSummary>
+                        <ExpansionPanelDetails>
+                            <Grid container spacing={24}>
+                                <Grid item xs={12}>
+                                    <Paper className={classes.paper}><ActiveElection></ActiveElection></Paper>
+                                </Grid>
+                            </Grid>
+
+                        </ExpansionPanelDetails>
+                    </ExpansionPanel>
+
+
+
                 </div>
             </div>
 
