dcde0dce2e700073f930bdefe0a6a37f65f2b6ff
asela <123azeela@gmail.com>
2019-01-16 22:19:29 +0530

    redux fully integrated

--- src/index.js
+++ src/index.js
@@ -2,13 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Index from './pages/index';
 import * as serviceWorker from './serviceWorker';
+
+import { Provider } from 'react-redux'
+import { createStore } from 'redux';
+import store from "./store/store";
+
+
 const axios = require('axios');
 
 axios.defaults.baseURL = 'http://localhost:9001/ec-election/';
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
 
 
-ReactDOM.render(<Index />, document.getElementById('root'));
+ReactDOM.render(
+    <Provider store={store} >
+        <Index />
+    </Provider >,
+    document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
