4a803c1a8a5e2d0ca1752ea231be840b9a308e45
thinkholic <mailtokasun@gmail.com>
2019-01-16 22:28:27 +0530

    Add ExpansionPanel component

--- src/stories/_data.js
+++ src/stories/_data.js
@@ -46,3 +46,25 @@ export const tableData = [
     { text: "Address goes here!", align: "center" }
   ]
 ];
+
+// Expansion Panel
+export const expansionPanelData = [
+  {
+    summary: "Expansion Panel 1",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+    disabled: false
+  },
+  {
+    summary: "Expansion Panel 2",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+    disabled: false
+  },
+  {
+    summary: "Expansion Panel 3 (Disabled)",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse malesuada lacus ex, sit amet blandit leo lobortis eget.",
+    disabled: true
+  }
+];
--- src/stories/index.js
+++ src/stories/index.js
@@ -7,11 +7,16 @@ import { storiesOf } from "@storybook/react";
 import DatePickers from "../components/DatePicker";
 import Progress from "../components/Progress/Progress";
 import Table from "../components/common/Table";
+import ExpansionPanel from "../components/common/ExpansionPanel";
 
-import { tableHeadings, tableData } from "./_data";
+import { tableHeadings, tableData, expansionPanelData } from "./_data";
 
 storiesOf("Table", module).add("default", () => (
   <Table headings={tableHeadings} rows={tableData} />
+));
+
+storiesOf("ExpansionPanel", module).add("default", () => (
+  <ExpansionPanel data={expansionPanelData} />
 ));
 
 storiesOf("DatePicker", module).add("default", () => <DatePickers />);
