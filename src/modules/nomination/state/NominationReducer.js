37ff98f7d5fab3a86239c36d1f63457f243c7331
Chamath Bogahawatta <Chamath.Bogahawatta@trycake.com>
2019-02-03 03:01:32 +0530

    (https://github.com/ECLK/Nomination-UI/issues/11) nomination review UI

--- src/modules/nomination/state/NominationReducer.js
+++ src/modules/nomination/state/NominationReducer.js
@@ -1,30 +1,49 @@
+import update from 'immutability-helper';
 import {
-    GET_NOMINATIONS,
-    POST_NOMINATION_PAYMENTS
+  GET_NOMINATIONS,
+  POST_NOMINATION_PAYMENTS,
+  NOMINATIONS_LOADED,
+  ON_NOMINATION_APPROVAL_CHANGE,
 } from "./NominationTypes";
 
 const initialState = {
-    //define the common states only
-    all_nominations: [3],
-    candidatePayments:[]
+  //define the common states only
+  all_nominations: [3],
+  candidatePayments:[],
+  nominations: [],
+};
+
+const findIndex = (nominations, id) => {
+  return nominations.findIndex(x => x.nomination_id === id);
 };
 
 export default function reducer(state = initialState, action) {
-    switch (action.type) {
-        case GET_NOMINATIONS:
-            debugger;
-            return {
-                ...state,
-                all_nominations: action.payload
-            };
-            case POST_NOMINATION_PAYMENTS:
-            return {
-                ...state,
-                candidatePayments: action.payload
-            };
+  switch (action.type) {
+    case GET_NOMINATIONS:
+      debugger;
+      return {
+        ...state,
+        all_nominations: action.payload
+      };
+    case POST_NOMINATION_PAYMENTS:
+      return {
+        ...state,
+        candidatePayments: action.payload
+      };
+    case NOMINATIONS_LOADED:
+      return {
+        ...state,
+        nominations: action.payload
+      };
+    case ON_NOMINATION_APPROVAL_CHANGE:
+      const nominations = state.nominations;
+      const index = findIndex(nominations, action.payload.id);
+      return {
+        ...state,
+        nominations: update(nominations, {[index]: {approval_status: {$set: action.payload.status}}})
+      }
 
-
-    }
-    return state;
+  }
+  return state;
 }
 
