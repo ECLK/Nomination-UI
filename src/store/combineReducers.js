beaca9f9b030432e087be1f7d7dda46c86bc9336
Chamath Bogahawatta <Chamath.Bogahawatta@trycake.com>
2019-03-04 01:05:39 +0530

    issue#(https://github.com/ECLK/Nomination-UI/issues/14) objection review ui first cut

--- src/store/combineReducers.js
+++ src/store/combineReducers.js
@@ -3,12 +3,13 @@ import { combineReducers } from "redux";
 import Nomination from "../modules/nomination/state/NominationReducer.js";
 import Payment from "../modules/payment/state/PaymentReducer.js";
 import Election from "../modules/election/state/ElectionReducer.js";
+import Objection from "../modules/objections/state/ObjectionReducer";
 
 
 export default combineReducers({
     Nomination,
     Payment,
     Election,
-
+    Objection,
     //other reducers
 });
