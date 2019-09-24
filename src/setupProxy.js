b633e0d207d84e464578877d691b7c0a6c9faac2
Kosala Gangabadage <kosalag@opensource.lk>
2019-09-24 20:37:03 +0530

    enable proxy debug

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,7 +2,7 @@ const proxy = require('http-proxy-middleware');
 import {AUTH_BASE_URL} from "./config.js";
 
 module.exports = function(app) {
-  app.use(proxy('/auth/callback', { target: AUTH_BASE_URL }));
-  app.use(proxy('/signin', { target: AUTH_BASE_URL }));
-  app.use(proxy('/create-pdf', { target: 'http://localhost:5000/' }));
+  app.use(proxy('/auth/callback', { target: AUTH_BASE_URL , logLevel:'debug'}));
+  app.use(proxy('/signin', { target: AUTH_BASE_URL , logLevel:'debug'}));
+  app.use(proxy('/create-pdf', { target: 'http://localhost:5000/' , logLevel:'debug' }));
 };
