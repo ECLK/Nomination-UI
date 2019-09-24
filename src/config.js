002e4cdd3af0c183d9df565215d3e17e2137321b
Kosala Gangabadage <kosalag@opensource.lk>
2019-09-24 19:37:39 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ console.log("process.env",process.env);
 // export const API_BASE_URL = 'http://localhost:9001/ec-election';
 // export const API_BASE_URL = 'https://api.nominations.ecdev.opensource.lk/ec-election';
 // export const API_BASE_URL = 'http://localhost:8280/nominations/0.9';
-export const API_BASE_URL = 'https://apim.ecdev.opensource.lk:8280/nominations/0.9';
+export const API_BASE_URL = 'https://apim.ecdev.opensource.lk/nominations/0.9';
 export const PDF_GENARATION_SERVICE_URL = 'http://localhost:5000/';
-export const AUTH_BASE_URL = 'https://authnominations.ecdev.opensource.lk:3001/';
+export const AUTH_BASE_URL = 'https://authnominations.ecdev.opensource.lk';
 
 
 // var API_BASE_URL = "https://nominations.ecdev.opensource.lk/ec-election";
