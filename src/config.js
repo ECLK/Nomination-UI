8c1c5f179581a32add95138a6b9c0448a4c915b5
Yujith Isura <yujith@Yujiths-MacBook-Pro.local>
2019-10-02 15:45:47 +0530

    Docker file added for staging env

--- src/config.js
+++ src/config.js
@@ -4,9 +4,9 @@ var AUTH_BASE_URL = 'http://localhost:3001/';
 
 
 if (process.env.NODE_ENV === 'production'){
-    API_BASE_URL = 'https://apim-gateway.ecdev.opensource.lk/nominations/0.9';
-    PDF_GENARATION_SERVICE_URL = 'http://localhost:5000/';
-    AUTH_BASE_URL = 'https://authnominations.ecdev.opensource.lk';
+    API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+    PDF_GENARATION_SERVICE_URL = process.env.REACT_APP_PDF_GENARATION_SERVICE_URL;
+    AUTH_BASE_URL = process.env.REACT_APP_AUTH_BASE_URL;
 }
 
 module.exports = {
