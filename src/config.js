2339c730d9719c28a55512bf264e07444c73fa8b
Yujith Isura <yujith@Yujiths-MacBook-Pro.local>
2019-10-03 12:31:17 +0530

    staging test4

--- src/config.js
+++ src/config.js
@@ -3,14 +3,19 @@ var PDF_GENARATION_SERVICE_URL = 'http://localhost:5000/';
 var AUTH_BASE_URL = 'http://localhost:3001/';
 
 
-if (process.env.NODE_ENV === 'production'){
-    API_BASE_URL = 'https://apim-gw.ecstag.opensource.lk/nominations/0.9';
-    PDF_GENARATION_SERVICE_URL = 'http://nominations-pdf-20keps.pxe-dev-platformer-1552477983757-1pdna.svc';
-    AUTH_BASE_URL = 'https://authnominations.ecstag.opensource.lk';
-}
+// if (process.env.NODE_ENV === 'production'){
+//     API_BASE_URL = 'https://apim-gw.ecstag.opensource.lk/nominations/0.9';
+//     PDF_GENARATION_SERVICE_URL = 'http://nominations-pdf-20keps.pxe-dev-platformer-1552477983757-1pdna.svc';
+//     AUTH_BASE_URL = 'https://authnominations.ecstag.opensource.lk';
+// }
 
 module.exports = {
     API_BASE_URL,
     PDF_GENARATION_SERVICE_URL,
     AUTH_BASE_URL
 }
+// export {
+//     API_BASE_URL,
+//     PDF_GENARATION_SERVICE_URL,
+//     AUTH_BASE_URL
+// }
